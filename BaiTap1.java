e447e7d6113d1b1ed9ceaf6376b3fd6c0b98429c
ChinhNTT <chinhntt.hust@gmail.com>
2023-08-16 18:03:39 +0700

    selenium

---
+++ BaiTap1.java
@@ -0,0 +1,9 @@
+package JavaBasic;
+public class BaiTap1 {
+    public static void main(String[] args) {
+        int a = 1123;
+        int b = 6745;
+        int c = 500;
+    System.out.println("(a*b) / c = " + ((a*b) / c));
+    }
+}
